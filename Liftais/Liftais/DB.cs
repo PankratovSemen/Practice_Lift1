6c4ad548bcbbc29f3f00e74ca432adbb6c297125
PankratovSemen <simen204@mail.ru>
2022-10-18 23:23:40 +0800

    First change
    Мое первое изменение

---
+++ Liftais/Liftais/DB.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Liftais
+{
+    internal class DB
+    {
+        MySqlConnection conn = new MySqlConnection("server = 192.168.0.150; port = 3307; username = root; password=root; database = lift");
+
+        public void openconn()
+        {
+            if (conn.State == System.Data.ConnectionState.Closed)
+                conn.Open();
+
+        }
+        public void closedconn()
+        {
+            if (conn.State == System.Data.ConnectionState.Open)
+                conn.Close();
+        }
+
+        public MySqlConnection getconn()
+        {
+            return conn;
+        }
+    } 
+}
---
+++ Liftais/Liftais/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Liftais
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SingIn_Click(object sender, RoutedEventArgs e)
+        {
+            String logins = login.Text;
+            String passwords = Password1.Password;
+            DB db = new DB();
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlCommand command = new MySqlCommand("SELECT * FROM usr WHERE login = @lg AND password = @ps", db.getconn());
+            command.Parameters.Add("@lg", MySqlDbType.VarChar).Value= logins;
+            command.Parameters.Add("@ps", MySqlDbType.VarChar).Value = passwords;
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+            {
+                Ais ais1 = new Ais();
+                ais1.Show();
+                this.Close();
+            }
+            else
+                MessageBox.Show("Неправильный логин или пароль","Error");
+                
+
+
+        }
+    }
+}
